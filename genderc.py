ce52ea90c48333a465b3b2291868df76a65b254f
Anirud <anirudt@gmail.com>
2014-12-17 03:13:30 +0530

    Updated gender c description

--- genderc.py
+++ genderc.py
@@ -1,10 +1,17 @@
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
+#above is the line for the attribute to 'item'
+
+'''
+A simple gender classifier based on the last letter
+'''
 import nltk
 from nltk.classify import NaiveBayesClassifier
 
 def gender_features(n):
 	return {'last letter': n[-1]}
 from nltk.corpus import names
+
+
 labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name,'female') for name in names.words('female.txt')])
 import random
 random.shuffle(labeled_names)
@@ -14,5 +21,11 @@ train_set = featuresets[:500]
 test_set = featuresets[500:]
 #classifier = nltk.classify.naivebayes.train(train_set)
 classifier = NaiveBayesClassifier.train(train_set)
-print classifier.classify(gender_features('Dickshitha'))
+print classifier.classify(gender_features('shyama'))
 print(nltk.classify.accuracy(classifier, test_set))
+classifier.show_most_informative_features(5)
+# Will indicate the most informative features
+
+
+
+    
